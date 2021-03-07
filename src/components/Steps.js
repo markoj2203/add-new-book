87df740941590ea173dca288f77efdc57a0fa29c
MARKO JOVICIC <markoj2203@gmail.com>
2021-03-08 00:03:32 +0100

    working with steps

--- src/components/Steps.js
+++ src/components/Steps.js
@@ -1,14 +1,34 @@
 import React from "react";
 import { useSelector } from "react-redux";
-import Stepper from "@material-ui/core/Stepper";
+/*import Stepper from "@material-ui/core/Stepper";
 import Step from "@material-ui/core/Step";
-import StepLabel from "@material-ui/core/StepLabel";
+import StepLabel from "@material-ui/core/StepLabel";*/
+import Stepper from "react-stepper-horizontal";
+import img from "../img/three-dots.svg";
 
 export default function Steps() {
   const activeStep = useSelector((state) => state.setActiveStep.count);
+
+  const steps =
+    activeStep < 2
+      ? [{ title: "Genre" }, { title: "Subgenre" }, { title: "", icon: img }]
+      : [
+          { title: "Genre" },
+          { title: "Subgenre" },
+          { title: "Add new subgenre" },
+          { title: "Information" },
+        ];
+
   return (
     <div>
-      <Stepper activeStep={activeStep} alternativeLabel>
+      <Stepper
+        steps={steps}
+        activeStep={activeStep}
+        size={50}
+        activeColor="#6c757d"
+        completeColor="lightgray"
+      />
+      {/* <Stepper activeStep={activeStep} alternativeLabel>
         <Step>
           <StepLabel>Genre</StepLabel>
         </Step>
@@ -16,12 +36,14 @@ export default function Steps() {
           <StepLabel>Subgenre</StepLabel>
         </Step>
         <Step>
-          <StepLabel>Add new subgenre</StepLabel>
+          <StepLabel icon={activeStep < 2 ? "..." : activeStep}>
+            {activeStep < 2 ? "" : "Add new subgenre"}
+          </StepLabel>
         </Step>
-        <Step>
-          <StepLabel>Information</StepLabel>
+        <Step disabled={true}>
+          <StepLabel>{activeStep < 2 ? "" : "Information"}</StepLabel>
         </Step>
-      </Stepper>
+      </Stepper> */}
     </div>
   );
 }
